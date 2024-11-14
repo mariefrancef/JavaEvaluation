c1e05f412d08497bcb3e64d983e342f1f9d419c2
Marie-France <147597283+mariefrancef@users.noreply.github.com>
2024-11-14 09:32:20 +0100

    initialize project

---
+++ EvalJava/src/main/java/com/example/demo/EvalJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvalJavaApplication.class, args);
+	}
+
+}
